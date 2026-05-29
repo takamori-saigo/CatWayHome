9bc9b5ebd3d123d6b42ca3114f14fb1c4552a3ca
takamori-saigo <anamneshev1999@gmail.com>
2026-05-29 20:17:19 +0500

    made collision for cat

---
+++ CatSWayHome/Models/Entities/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace CatSWayHome.Models;
+
+public abstract class BaseEntity
+{
+    public Vector2 Position { get; set; }
+    public virtual int PositionGround { get; set; } = 690;
+    public Vector2 WorldPosition{ get; set; }
+    public float ParallaxFactor { get; set; } = 1.8f;
+    public float WidthTexture { get; set; }
+    public float HeightTexture { get; set; }
+    public void Update(int deltaX)
+    {
+        Position = new Vector2(WorldPosition.X - deltaX * ParallaxFactor, PositionGround);
+    }
+}
