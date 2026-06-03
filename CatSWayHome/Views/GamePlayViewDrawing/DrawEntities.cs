44418cff858d1d92fc139b113c15a1c9720c37f8
takamori-saigo <anamneshev1999@gmail.com>
2026-06-03 20:06:23 +0500

    refactored drawing of entity

--- CatSWayHome/Views/GamePlayViewDrawing/DrawEntities.cs
+++ CatSWayHome/Views/GamePlayViewDrawing/DrawEntities.cs
@@ -30,91 +30,45 @@ public class DrawEntities: IDrawElement
     {
         foreach (var e in _entities)
         {
-            var scale = 1f;
             switch (e)
             {
                 case Rubbish:
                 {
-                    var rub = e;
-                    scale = 0.45f;
-                    /*if (rub.HeightTexture == 0)
-                    {
-                        rub.HeightTexture = _rubbish.Height * scale;
-                        rub.WidthTexture = _rubbish.Width * scale ;
-                        rub.HitBoxPoisitionY = 100;
-                    }*/
-                    var temp = new Vector2(rub.Position.X, rub.Position.Y - rub.HitBoxPoisitionY);
-                    DebugClassHitbox.DrawHitBox(temp, _spriteBatch, (int)rub.WidthTexture,
-                        (int)rub.HeightTexture + rub.HitBoxPoisitionY);
-                    _spriteBatch.Draw(_rubbish, rub.Position, null, Color.White, 0f, Vector2.Zero, scale,
-                        SpriteEffects.None, 0f);
+                    DrawCurrentEntity(e as Rubbish, 0.45f, _cucumber);
                     break;
                 }
                 case Cucumber:
-                    var cuc = e;
-                    scale = 0.07f;
-
-                    if (cuc.HeightTexture == 0)
-                    {
-                        cuc.HeightTexture = _cucumber.Height * scale;
-                        cuc.WidthTexture = _cucumber.Width * scale;
-                    }
-
-                    /*
-                    DebugClassHitbox.DrawHitBox(cuc.Position, _spriteBatch, (int)cuc.WidthTexture, (int)cuc.HeightTexture);
-                    */
-
-                    _spriteBatch.Draw(_cucumber, cuc.Position, null, Color.White, 0f, Vector2.Zero, scale,
-                        SpriteEffects.None, 0f);
+                    DrawCurrentEntity(e as Cucumber, 0.07f, _cucumber);
                     break;
                 case Bench:
-                    var bench = e;
-                    scale = 0.4f;
-                    if (bench.HeightTexture == 0)
-                    {
-                        bench.HeightTexture = _bench.Height * scale;
-                        bench.WidthTexture = _bench.Width * scale;
-                    }
-
-                    DebugClassHitbox.DrawHitBox(bench.Position, _spriteBatch, (int)bench.WidthTexture,
-                        (int)bench.HeightTexture);
-                    _spriteBatch.Draw(_bench, bench.Position, null, Color.White, 0f, Vector2.Zero, scale,
-                        SpriteEffects.None, 0f);
+                    DrawCurrentEntity(e as Bench,  0.4f, _cucumber);
                     break;
                 case Candei:
-                    var candei = e;
-                    scale = 0.4f;
-                    if (candei.HeightTexture == 0)
-                    {
-                        candei.HeightTexture = _candei.Height * scale;
-                        candei.WidthTexture = _candei.Width * scale;
-                    }
-
-                    DebugClassHitbox.DrawHitBox(candei.Position, _spriteBatch, (int)candei.WidthTexture,
-                        (int)candei.HeightTexture);
-                    _spriteBatch.Draw(_candei, candei.Position, null, Color.White, 0f, Vector2.Zero, scale,
-                        SpriteEffects.None, 0f);
+                    DrawCurrentEntity(e as Candei,  0.4f, _cucumber);
                     break;
                 case Fish:
                     var fish = e as Fish;
-                    scale = 0.07f;
-                    if (fish.HeightTexture == 0)
-                    {
-                        fish.HeightTexture = _candei.Height * scale;
-                        fish.WidthTexture = _candei.Width * scale;
-                    }
-                    
                     if (!fish.Take)
-                    {
-                        DebugClassHitbox.DrawHitBox(fish.Position, _spriteBatch, (int)fish.WidthTexture,
-                            (int)fish.HeightTexture);
-                        _spriteBatch.Draw(_fish, fish.Position, null, Color.White, 0f, Vector2.Zero, scale,
-                            SpriteEffects.None, 0f);
-                    }
+                        DrawCurrentEntity(fish, 0.07f, _cucumber);
                     break;
             }
         }
         
+    }
+
+    private void DrawCurrentEntity(BaseEntity entity, float scale, Texture2D texture)
+    {
+
+        if (entity.HeightTexture == 0)
+        {
+            entity.HeightTexture = texture.Height * scale;
+            entity.WidthTexture = texture.Width * scale;
+        }
+
+        /*DebugClassHitbox.DrawHitBox(entity.Position, _spriteBatch, (int)entity.WidthTexture,
+            (int)entity.HeightTexture);*/
+        _spriteBatch.Draw(texture, entity.Position, null, Color.White, 0f, Vector2.Zero, scale,
+            SpriteEffects.None, 0f);
     }
     
     public void LoadContent()
